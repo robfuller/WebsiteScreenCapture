8987ef3b0943747283b4e9faba87f54e3e9f6120
Rob Fuller <robfuller@gmail.com>
2010-11-12 14:39:44 -0500

    new file:   ScreenCaptureMagic/FileBasedRun.cs 	new file:   ScreenCaptureMagic/GenerateLists.cs 	new file:   ScreenCaptureMagic/Util/StaticWebHelpers.cs 	modified:   ScreenCaptureMagic/AppStarter.Designer.cs 	modified:   ScreenCaptureMagic/AppStarter.cs 	modified:   ScreenCaptureMagic/Util/AppSettings.cs             added javascript follow option 	modified:   ScreenCaptureMagic/Program.cs 	    added list generator csv output calls 	deleted:    ScreenCaptureMagic/SpiderAndCapture.cs 	    refactored to no longer need 	modified:   ScreenCaptureMagic/Util/AppHelpers.cs 	modified:   ScreenCaptureMagic/Util/WebHelpers.cs

---
+++ ScreenCaptureMagic/FileBasedRun.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScreenCaptureMagic.Util;
+
+namespace ScreenCaptureMagic
+{
+    class FileBasedRun
+    {
+        private Models.SiteInfo _site_info = new Models.SiteInfo();
+        private Util.AppSettings _settings = new AppSettings();
+        private Util.WebHelpers _wbh;
+
+        public FileBasedRun(string file, AppSettings settings)
+        {
+            _settings = settings;
+        }
+        private void ExecuteProgram(string file)
+        {
+            _wbh = new Util.WebHelpers(_settings);
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(file))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    _site_info.Pages.Add(ProcessUrl(line.Trim()));
+                }
+            }
+            _wbh.Dispose();
+            
+        }
+        private Models.PageAssets ProcessUrl(string url)
+        {
+            try
+            {
+                Uri uri = new Uri(url);
+                
+                Models.PageAssets page;
+                page = _wbh.processPage(uri);
+                _site_info.Pages.Add(page);
+                if (_settings.TakeScreenShots)
+                {
+                    page.BitmapFilename = _wbh.takeScreenshot();
+                }
+                return page;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ ScreenCaptureMagic/GenerateLists.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ScreenCaptureMagic
+{
+    class GenerateLists
+    {
+        public static void CreateReport(Models.SiteInfo siteinfo)
+        {
+            createReportList_Urls(siteinfo.URIs);
+            createReportList_BadUrls(siteinfo.Bad_URLs);
+            createReportList_images(siteinfo.Pages);
+            createReportList_urlsByPage(siteinfo.Pages);
+        }
+
+        private static void createReportList_urlsByPage(List<Models.PageAssets> pages)
+        {
+            try
+            {
+                StreamWriter fs = new StreamWriter("links_by_page.csv", false);
+                fs.WriteLine("Page, URL");
+                foreach (Models.PageAssets page in pages)
+                {
+                    foreach (string url in page.All_links)
+                    {
+                        fs.WriteLine(page.Page_URL + ", " + url);
+                    }
+                }
+                fs.Close();
+                fs.Dispose();
+            }
+            catch
+            {
+                throw;
+            }
+            
+        }
+
+        private static void createReportList_Urls(List<Uri> urls)
+        {
+            try
+            {
+                StreamWriter fs = new StreamWriter("urls.csv", false);
+                fs.WriteLine( "URL");
+                foreach (Uri url in urls)
+                {
+                    fs.WriteLine(url.ToString());
+                }
+                fs.Close();
+                fs.Dispose();
+            }
+            catch 
+            { 
+                throw; 
+            }
+        }
+
+        private static void createReportList_BadUrls(List<Models.BadUrl> badurls)
+        {
+            try
+            {
+                StreamWriter fs = new StreamWriter("bad_urls.csv", false);
+                fs.WriteLine("Found on URL, Bad Url, Message");
+                foreach (Models.BadUrl  badurl in badurls)
+                {
+                    fs.WriteLine(badurl.FoundOnUrl + ", " + badurl.Url + ", " + badurl.Message);
+                }
+                fs.Close();
+                fs.Dispose();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static void createReportList_images(List<Models.PageAssets> pages)
+        {
+         try
+            {
+                StreamWriter fs = new StreamWriter("images_by_page.csv", false);
+                fs.WriteLine("Found on URL, image URL");
+                foreach (Models.PageAssets page in pages)
+                {
+                    foreach (string img in page.All_images)
+                    {
+                        fs.WriteLine(page.Page_URL + ", " + img);
+                    }
+              
+                }
+                fs.Close();
+                fs.Dispose();
+            }
+            catch
+            {
+                throw;
+            }
+        
+        }
+
+    }
+}
